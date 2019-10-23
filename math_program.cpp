b2b1062ea86c16ccece76f613be901c928c504c4
Ubuntu <ubuntu@ip-172-31-31-42.ec2.internal>
2019-10-23 23:58:43 +0000

    Initial commit

---
+++ math_program.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2019 Joseph Palermo All rights reserved.
+//
+// Created by: Joseph Palermo
+// Created on: October 2019
+// This program calculates area and perimeter
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    // this function calculates area and perimeter
+
+    // a=lw The Area Equation
+    std::cout << "5*3= " << (5*3) << std::endl;
+    // 2(l+w) The Perimeter Equation
+    std::cout << "2(5+3)= " << 2*(5+3) << std::endl;
+}
